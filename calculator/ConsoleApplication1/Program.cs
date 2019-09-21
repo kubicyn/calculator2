7ca606f90dd07aed3fae5f6a5a59deb58c92975f
Vampsas <sana222@autograf.pl>
2019-09-21 16:59:04 +0200

    First

--- calculator/ConsoleApplication1/Program.cs
+++ calculator/ConsoleApplication1/Program.cs
@@ -14,15 +14,13 @@ namespace ConsoleApplication1
             {
                 Console.WriteLine("1. Dodaj");
                 Console.WriteLine("2. Odejmij");
-<<<<<<< HEAD
                 Console.WriteLine("3. Integration");
                 int menuOption = Convert.ToInt16(Console.ReadLine());
-=======
+
                 Console.WriteLine("3. Mnożenie");
                 Console.WriteLine("4. Dzielenie.");
                 Console.WriteLine("5. Exit");
-                int menuOption = Convert.ToInt32(Console.ReadLine());
->>>>>>> f66b9b1... Add div option to menu
+                
                 if (menuOption == 3)
                     break;
             }
